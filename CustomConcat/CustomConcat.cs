25f112d02151e583ceab545501040d6e050a5788
kserditov <serditov@gmail.com>
2015-03-28 11:29:35 +0300

    Commit user defined aggregate for concatenation

---
+++ CustomConcat/CustomConcat.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+
+namespace CustomConcat
+{
+    [Serializable]
+    [Microsoft.SqlServer.Server.
+    SqlUserDefinedAggregate(
+    Format.UserDefined,
+    IsInvariantToNulls = true,
+    IsInvariantToDuplicates = true,
+    IsInvariantToOrder = false,
+    MaxByteSize = 8000)]
+
+    public struct CustomConcat : IBinarySerialize
+    {
+        private StringBuilder str;
+
+        public void Init()
+        {
+            str = new StringBuilder();
+        }
+
+        public void Accumulate ( SqlString value)
+        {
+            this.str.Append(value);
+        }
+
+        public void Merge(CustomConcat group)
+        {
+            this.str.Append(group);
+        }
+
+        public SqlString Terminate()
+        {
+            return new SqlString(str.ToString());
+        }
+
+        public void Read(System.IO.BinaryReader r)
+        {
+            str = new StringBuilder(r.ReadString());
+        }
+
+        public void Write(System.IO.BinaryWriter w)
+        {
+            w.Write(str.ToString());
+        }
+    }
+}
---
+++ USAGE.sql
@@ -0,0 +1,10 @@
+﻿CREATE ASSEMBLY ClrCustomConcatFunction 
+from 'C:\Users\Urich\Documents\Visual Studio 2013\Projects\CustomConcat\CustomConcat\bin\Release\CustomConcat.dll' ;
+
+CREATE AGGREGATE dbo.CustomConcat (@value nvarchar(max))
+RETURNS nvarchar(max)
+EXTERNAL NAME ClrCustomConcatFunction.[CustomConcat.CustomConcat];
+
+--Example of use
+SELECT dbo.CustomConcat(firstname) FROM HR.Employees
+GROUP BY mgrid;
